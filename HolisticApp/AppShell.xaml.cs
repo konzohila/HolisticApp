dec2cfb03ea63a79b92a79586bf73683a17bc9ee
konzohila <svenkonz@gmail.com>
2025-02-04 21:07:05 +0100

    initial commit

---
+++ HolisticApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HolisticApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ HolisticApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HolisticApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
